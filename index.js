72149e3f6f5b4646f54b989f57c4209720bea67d
Pauline122112 <pauline.design007@gmail.com>
2021-09-30 17:27:42 -0500

    finished index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require("dotenv").config
 
-const server = require("./api/server.js");
+const server = require("./api/server.js")
 
-const PORT = process.env.PORT;
+const PORT = 4000
 
 server.listen(PORT, () => {
-	console.log(" Server Running on ", PORT);
+	console.log(" Server Running on ", PORT)
 });
